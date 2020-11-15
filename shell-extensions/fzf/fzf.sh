17f857054304d480f8cbdeb0287ac544f7375972
Renato Rufino <renators@gmail.com>
2020-11-15 01:25:10 -0300

    stops using my fork of fzf-z
    back then i forked this plugin to use the fzfcmd instead of
    hardcoded fzf since i used the fzf-tmux script integration and wanted to
    take advantage of it. since the original author implemented a way
    though global vars to update the fzf-z fzf cmd i can safely switch back.
    
    its not maintaned anymore, but i prefer, for now to use this combination
    of z and fzf-z. might look into switching to fasd and fzf-fasd

--- shell-extensions/fzf/fzf.sh
+++ shell-extensions/fzf/fzf.sh
@@ -1,6 +1,7 @@
 export FZF_TMUX=1
 export FZF_DEFAULT_COMMAND='ag -g ""'
 export FZF_TMUX_HEIGHT='40%'
+export FZF_BIN_PATH=fzf-tmux
 export ENHANCD_FILTER=fzf-tmux
 # To apply the command to CTRL-T as well
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
