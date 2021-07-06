92f9f6e27bc49c71d593007bcda034947d50208f
Renato Rufino <renators@gmail.com>
2021-07-06 16:04:05 -0300

    fix: prevent tsserver from disabling lsp_signature

--- config/nvim/lua/lang/javascript.lua
+++ config/nvim/lua/lang/javascript.lua
@@ -29,7 +29,7 @@ local tsserver_config = {
         no_save_after_format = false,
         -- parentheses completion
         complete_parens = false,
-        signature_help_in_parens = true,
+        signature_help_in_parens = false,
         update_imports_on_move = true,
         require_confirmation_on_move = true,
         watch_dir = "./"
