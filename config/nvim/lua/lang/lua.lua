c334ae5c62f283550e30fbb1acb1fbb773a262e9
Renato Rufino <renators@gmail.com>
2021-10-26 17:07:55 -0300

    feat: improve lua lspconfig

--- config/nvim/lua/lang/lua.lua
+++ config/nvim/lua/lang/lua.lua
@@ -2,6 +2,10 @@ local config = require('lang.config')
 
 local lua_lsp_config ={
     -- available in path (installed through arch package manager)
+    on_attach = function(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        config.default_on_attach(client, bufnr)
+    end,
     cmd = {"lua-language-server"},
     settings = {
         Lua = {
@@ -19,7 +23,8 @@ local lua_lsp_config ={
                 -- Make the server aware of Neovim runtime files
                 library = {
                     [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-                    [vim.fn.expand("$VIMRUNTIME/lua/lang")] = true
+                    [vim.fn.expand("$VIMRUNTIME/lua/lang")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
                 }
             }
         }
