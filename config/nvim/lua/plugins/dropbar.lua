5b4658fb0e3e4124e14fef6efbb44c3388178134
Renato Rufino (Kibe) <renators@gmail.com>
2023-09-09 14:51:16 -0300

    fix: commiting missing plugin setup initialization files

---
+++ config/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+M.setup = function ()
+    -- local opts = {
+    --     general = {
+    --         update_interval = 50
+    --     }
+    -- }
+   -- require("dropbar").setup(opts)
+end
+
+return M;
